de5c1176cbaa34dfcdc52d26c8d18cddec03496f
Rahuvich <raulmateob@gmail.com>
2018-11-27 00:13:35 +0100

    Added map

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([41.462283, 2.269219], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([41.462283, 2.269219]).addTo(map)
+    .bindPopup('Estamos aquí!')
+    .openPopup();
